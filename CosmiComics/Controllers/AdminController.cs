8a7d18728eebab7ffb9676633dfcf94be6e05f35
MarcoKruger <42533098+MarcoKruger@users.noreply.github.com>
2018-08-20 22:11:33 -0700

    Third Commit

---
+++ CosmiComics/Controllers/AdminController.cs
@@ -0,0 +1,44 @@
+﻿using CosmiComics.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CosmiComics.Controllers
+{
+    public class AdminController : Controller
+    {
+        //ADmin login actions
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login([Bind(Include = "Username,Password")]AdminLogin adminLogin)
+        {
+            if (ModelState.IsValid)
+            {
+                if (adminLogin.Username == "Admin")
+                {
+                    if (adminLogin.Password == "Admin")
+                    {
+                        return RedirectToAction("AdminIndex", "Admin");
+                    }
+                    return View();
+                }
+                return View();
+            }
+            return View();
+        }
+        // Admin page afer he logs in
+        private ProductDBContext db = new ProductDBContext();
+        public ActionResult AdminIndex()
+        {
+            ViewBag.MusicView = db.Music.ToList();
+            ViewBag.MovieView = db.Movies.ToList();
+            ViewBag.BookView = db.Books.ToList();
+            return View();
+        }
+    }
+}
---
+++ CosmiComics/Models/AdminLogin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CosmiComics.Models
+{
+    public class AdminLogin
+    {
+        [Required(ErrorMessage = "No username given")]
+        [Display(Name = "Admin Username")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "No password given")]
+        [Display(Name = "Admin Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
