c590b766b78cfcd669e9f39aadd51b31f7429a49
MarcoKruger <42533098+MarcoKruger@users.noreply.github.com>
2018-08-20 21:39:24 -0700

    Second Commit

--- CosmiComics/Global.asax.cs
+++ CosmiComics/Global.asax.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Data.Entity;
+using CosmiComics.Models;
 
 namespace CosmiComics
 {
@@ -12,6 +14,7 @@ namespace CosmiComics
     {
         protected void Application_Start()
         {
+            Database.SetInitializer(new ProductDBInitializer());
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
--- CosmiComics/Models/ProductDBInitializer.cs
+++ CosmiComics/Models/ProductDBInitializer.cs
@@ -2,10 +2,85 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
 
 namespace CosmiComics.Models
 {
-    public class ProductDBInitializer
+    public class ProductDBInitializer : DropCreateDatabaseAlways<ProductDBContext>
     {
+        protected override void Seed(ProductDBContext context)
+        {
+            // Books
+            context.Books.Add(new Book
+            {
+                Name = "Seven seas",
+                Description = "Story about dragons",
+                Image = "",
+                Price = 320
+            });
+            context.Books.Add(new Book
+            {
+
+            });
+            context.Books.Add(new Book
+            {
+
+            });
+            context.Books.Add(new Book
+            {
+
+            });
+            context.Books.Add(new Book
+            {
+
+            });
+            context.Books.Add(new Book
+            {
+
+            });
+            // Movies
+            context.Movies.Add(new Movie
+            {
+
+            });
+            context.Movies.Add(new Movie
+            {
+
+            });
+            context.Movies.Add(new Movie
+            {
+
+            });
+            context.Movies.Add(new Movie
+            {
+
+            });
+            context.Movies.Add(new Movie
+            {
+
+            });
+            // Music
+            context.Music.Add(new Music
+            {
+
+            });
+            context.Music.Add(new Music
+            {
+
+            });
+            context.Music.Add(new Music
+            {
+
+            });
+            context.Music.Add(new Music
+            {
+
+            });
+            context.Music.Add(new Music
+            {
+
+            });
+            base.Seed(context);
+        }
     }
 }
