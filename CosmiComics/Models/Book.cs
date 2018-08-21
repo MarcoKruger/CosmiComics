cc3bf258ae402bf050ea974a98a2d936904334b1
MarcoKruger <42533098+MarcoKruger@users.noreply.github.com>
2018-08-20 21:21:04 -0700

    First Commit

---
+++ CosmiComics/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CosmiComics.Models
+{
+    public class Book
+    {
+        public virtual int BookId { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string Description { get; set; }
+        public virtual string Image { get; set; }
+        public virtual decimal Price { get; set; }
+    }
+}
---
+++ CosmiComics/Models/ProductDBContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CosmiComics.Models
+{
+    public class ProductDBContext : DbContext
+    {
+        public virtual DbSet<Book> Books { get; set; }
+        public virtual DbSet<Music> Music { get; set; }
+        public virtual DbSet<Movie> Movies { get; set; }
+    }
+}
